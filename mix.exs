6c27a0cbf6ef87e513cea26c24f4b6592258dedc
zwpdbh <hyperion_z@outlook.com>
2025-07-27 12:53:01 +0800

    > debug  mix deps.compile exla

--- mix.exs
+++ mix.exs
@@ -76,7 +76,7 @@ defmodule Hello.MixProject do
       {:dotenvy, "~> 1.1.0"},
       {:pgvector, "~> 0.3.0"},
       {:bumblebee, path: "~/code/elixir_programming/examples/bumblebee"},
-      {:exla, ">= 0.0.0"},
+      {:exla, ">= 0.10.0"},
       {:text_chunker, "~> 0.4.0"}
     ]
   end
