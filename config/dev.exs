6339f43313f6e63e59fc5770d10b8e95cd213e84
zwpdbh <hyperion_z@outlook.com>
2025-08-01 09:55:10 +0800

    updated to liveview 1.1

--- config/dev.exs
+++ config/dev.exs
@@ -81,7 +81,8 @@ config :phoenix_live_view,
   # Changing this configuration will require mix clean and a full recompile.
   debug_heex_annotations: true,
   # Enable helpful, but potentially expensive runtime checks
-  enable_expensive_runtime_checks: true
+  enable_expensive_runtime_checks: true,
+  debug_attributes: true
 
 # Disable swoosh api client as it is only required for production adapters.
 config :swoosh, :api_client, false
--- mix.exs
+++ mix.exs
@@ -11,7 +11,8 @@ defmodule Hello.MixProject do
       aliases: aliases(),
       deps: deps(),
       listeners: [Phoenix.CodeReloader],
-      consolidate_protocols: Mix.env() != :dev
+      consolidate_protocols: Mix.env() != :dev,
+      compilers: [:phoenix_live_view] ++ Mix.compilers()
     ]
   end
 
@@ -52,8 +53,8 @@ defmodule Hello.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 1.0.17"},
-      {:floki, ">= 0.30.0", only: :test},
+      {:phoenix_live_view, "~> 1.1"},
+      {:lazy_html, ">= 0.0.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:esbuild, "~> 0.9", runtime: Mix.env() == :dev},
       {:tailwind, "~> 0.3", runtime: Mix.env() == :dev},
