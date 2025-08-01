cdf74ca31ebd15e3ae406740bc41160a4ddcdd5a
zwpdbh <hyperion_z@outlook.com>
2025-08-01 19:21:21 +0800

    > finish my resume

--- lib/hello_web/live/tailwind_live/demo04.ex
+++ lib/hello_web/live/tailwind_live/demo04.ex
@@ -9,8 +9,98 @@ defmodule HelloWeb.TailwindLive.Demo04 do
   @impl true
   def render(assigns) do
     ~H"""
-    <div>
-      My Resume
+    <div class="p-20 bg-white grid grid-cols-6 gap-4 text-gray-800">
+      <div class="divide-y divide-gray-300">
+        <div>
+          <div class="text-3xl font-bold">Wei Zhao</div>
+          <div class="mb-8">hyperion_z@outlook.com</div>
+          <div>
+            Fullstack developer who use Elixir to find myself.
+          </div>
+        </div>
+        <div class="space-y-4">
+          <div class="text-2xl font-bold">About</div>
+          <div>
+            <div class="font-sm">Main Skill Stack</div>
+            <div class="font-bold">Elixir, Rust, JavaScript, Azure, AWS</div>
+          </div>
+          <div>
+            <div class="font-sm">Telephone</div>
+            <div class="font-bold">(+086)15811257483</div>
+          </div>
+        </div>
+      </div>
+      <div class="col-start-2 col-span-5 space-y-2">
+        <div class="container max-auto">
+          <div class="relative bg-white p-7 shadow-lg rounded">
+            <div class="absolute top-0 left-0 text-lg font-bold rounded px-4 py-1 transform -translate-x-2 -translate-y-2">
+              Current Status
+            </div>
+            <div>
+              Chupa chups powder wafer marzipan gingerbread oat cake. Chocolate pudding carrot cake donut dragée dessert. Tootsie roll pie brownie donut candy ice cream tiramisu lemon drops. Halvah lollipop gummi bears liquorice fruitcake. Oat cake cheesecake liquorice powder
+            </div>
+          </div>
+        </div>
+        <div class="container max-auto">
+          <div class="relative bg-white p-7 shadow-lg rounded">
+            <div class="absolute top-0 left-0 text-lg font-bold rounded px-4 py-1 transform -translate-x-2 -translate-y-2">
+              Project Experimence
+            </div>
+            <div class="space-y-5">
+              <div>
+                <div class="transform -translate-x-4 flex items-center divide-x divide-gray-300">
+                  <div class="font-semibold pr-3">Azure Container Storage Test</div>
+                  <div class="flex pl-3 space-x-3">
+                    <div class="text-sm font-medium italic">Microsft</div>
+                    <div class="text-sm font-light">2021 -- 2025</div>
+                  </div>
+                </div>
+                <div>
+                  Chupa chups powder wafer marzipan gingerbread oat cake. Chocolate pudding carrot cake donut dragée dessert. Tootsie roll pie brownie donut candy ice cream tiramisu lemon drops. Halvah lollipop gummi bears liquorice fruitcake. Oat cake cheesecake liquorice powder
+                </div>
+              </div>
+
+              <div>
+                <div class="transform -translate-x-4 flex items-center divide-x divide-gray-300">
+                  <div class="font-semibold pr-3">Azure Container Storage Test</div>
+                  <div class="flex pl-3 space-x-3">
+                    <div class="text-sm font-medium italic">Microsft</div>
+                    <div class="text-sm font-light">2021 -- 2025</div>
+                  </div>
+                </div>
+                <div>
+                  Chupa chups powder wafer marzipan gingerbread oat cake. Chocolate pudding carrot cake donut dragée dessert. Tootsie roll pie brownie donut candy ice cream tiramisu lemon drops. Halvah lollipop gummi bears liquorice fruitcake. Oat cake cheesecake liquorice powder
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="container max-auto">
+          <div class="relative bg-white p-7 shadow-lg rounded">
+            <div class="absolute top-0 left-0 text-lg font-bold rounded px-4 py-1 transform -translate-x-2 -translate-y-2">
+              Education
+            </div>
+            <div class="space-y-4">
+              <div>
+                <div class="font-bold">Otago</div>
+                <div>Good student</div>
+              </div>
+              <div>
+                <div class="font-bold">Otago</div>
+                <div>Good student</div>
+              </div>
+              <div>
+                <div class="font-bold">Otago</div>
+                <div>Good student</div>
+              </div>
+              <div>
+                <div class="font-bold">Otago</div>
+                <div>Good student</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
     """
   end
