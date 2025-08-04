3d9892f2760e15a63a285c7c5c1dc8faacb430c4
zwpdbh <hyperion_z@outlook.com>
2025-08-04 20:16:59 +0800

    > fix layout when there is messages

--- lib/hello_web/live/tailwind_live/demo06.ex
+++ lib/hello_web/live/tailwind_live/demo06.ex
@@ -7,9 +7,9 @@ defmodule HelloWeb.TailwindLive.Demo06 do
       socket
       |> assign(:sidebar_open, true)
       |> assign(:messages, [
-        %{from: "bot", content: "this is cool"},
-        %{from: "user01", content: "I am noob"},
-        %{from: "user02", content: "It is fine"}
+        # %{from: "bot", content: "this is cool"},
+        # %{from: "user01", content: "I am noob"},
+        # %{from: "user02", content: "It is fine"}
       ])
 
     {:ok, socket}
@@ -57,29 +57,32 @@ defmodule HelloWeb.TailwindLive.Demo06 do
           <div class="hover:bg-gray-200 p-1 rounded">chat history 03</div>
         </div>
       </div>
-      
-    <!-- Main Content -->
-      <div class="flex-1 bg-gray-50 p-4 overflow-auto flex justify-center items-center">
-        <div class="w-6/8 h-full bg-orange-200">
-          <div class="bg-blue-200">
-            <.render_messages messages={@messages} />
-          </div>
-          <div class="bg-teal-200">
-            <form phx-submit="submit" class="border-t border-gray-200 p-4 space-y-2 ">
-              <textarea
-                id="content"
-                phx-hook="SubmitOnCmdEnter"
-                name="content"
-                class="block resize-none w-full border-gray-200 rounded-2xl bg-white focus:ring-0 focus:border-gray-300 focus:shadow-sm"
-                placeholder="Enter a message..."
-                rows={4}
-              ></textarea>
-              <div class="flex justify-end">
-                <button class="bg-gray-200 hover:bg-gray-300 transition px-3 py-1.5 rounded flex items-center justify-center">
-                  Send
-                </button>
+
+      <div class="flex-1 bg-gray-50 overflow-hidden flex flex-col">
+        <div class="p-4 flex-1 flex flex-col">
+          <div class={
+            if @messages == [],
+              do: "flex-1 flex items-center justify-center",
+              else: "flex-col"
+          }>
+            <%= if @messages != [] do %>
+              <div class="flex-1 overflow-y-auto bg-blue-200 rounded-lg p-2 mb-4">
+                <.render_messages messages={@messages} />
               </div>
-            </form>
+            <% end %>
+
+            <div class="w-4/5 rounded-2xl p-4 ">
+              <form phx-submit="submit">
+                <textarea
+                  id="content"
+                  phx-hook="SubmitOnCmdEnter"
+                  name="content"
+                  class="block resize-none w-full rounded-2xl bg-gray-100 p-4 placeholder-gray-400 placeholder:text-sm placeholder:italic border-none outline-none"
+                  placeholder="Enter a message..."
+                  rows="6"
+                />
+              </form>
+            </div>
           </div>
         </div>
       </div>
