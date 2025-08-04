8744b7a6390cf0b12157ad12785d1425414ef327
zwpdbh <hyperion_z@outlook.com>
2025-08-04 19:20:58 +0800

    > adjust main content for rendering messages

--- lib/hello_web/live/tailwind_live/demo06.ex
+++ lib/hello_web/live/tailwind_live/demo06.ex
@@ -3,7 +3,16 @@ defmodule HelloWeb.TailwindLive.Demo06 do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket |> assign(:sidebar_open, true)}
+    socket =
+      socket
+      |> assign(:sidebar_open, true)
+      |> assign(:messages, [
+        %{from: "bot", content: "this is cool"},
+        %{from: "user01", content: "I am noob"},
+        %{from: "user02", content: "It is fine"}
+      ])
+
+    {:ok, socket}
   end
 
   @impl true
@@ -31,7 +40,7 @@ defmodule HelloWeb.TailwindLive.Demo06 do
         </div>
         
     <!-- Sidebar Content (only visible when open) -->
-        <div :if={@sidebar_open} class="space-y-2">
+        <div :if={@sidebar_open} class="space-y-1">
           <div class="flex items-center gap-1 hover:bg-gray-200 p-1 rounded">
             <.icon name="hero-pencil-square" />
             <span>new chat</span>
@@ -50,8 +59,29 @@ defmodule HelloWeb.TailwindLive.Demo06 do
       </div>
       
     <!-- Main Content -->
-      <div class="flex-1 bg-gray-50 p-4 overflow-auto">
-        <div>let's chat</div>
+      <div class="flex-1 bg-gray-50 p-4 overflow-auto flex justify-center items-center">
+        <div class="w-6/8 h-full bg-orange-200">
+          <div class="bg-blue-200">
+            <.render_messages messages={@messages} />
+          </div>
+          <div class="bg-teal-200">
+            <form phx-submit="submit" class="border-t border-gray-200 p-4 space-y-2 ">
+              <textarea
+                id="content"
+                phx-hook="SubmitOnCmdEnter"
+                name="content"
+                class="block resize-none w-full border-gray-200 rounded-2xl bg-white focus:ring-0 focus:border-gray-300 focus:shadow-sm"
+                placeholder="Enter a message..."
+                rows={4}
+              ></textarea>
+              <div class="flex justify-end">
+                <button class="bg-gray-200 hover:bg-gray-300 transition px-3 py-1.5 rounded flex items-center justify-center">
+                  Send
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
     """
@@ -60,5 +90,16 @@ defmodule HelloWeb.TailwindLive.Demo06 do
   @impl true
   def handle_event("toggle_sidebar", _, socket) do
     {:noreply, update(socket, :sidebar_open, &(!&1))}
+  end
+
+  def render_messages(assigns) do
+    ~H"""
+    <%= for %{from: from, content: content} <- @messages do %>
+      <div class="flex">
+        <div>from: {from}</div>
+        <div>content: {content}</div>
+      </div>
+    <% end %>
+    """
   end
 end
