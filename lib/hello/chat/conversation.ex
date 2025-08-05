74428688be906b382cd12840d792f5c4b5ad0c7d
zwpdbh <hyperion_z@outlook.com>
2025-08-05 12:00:37 +0800

    compare with tunez chat and do crud for chat

--- lib/hello/chat/conversation.ex
+++ lib/hello/chat/conversation.ex
@@ -10,9 +10,32 @@ defmodule Hello.Chat.Conversation do
 
   attributes do
     uuid_primary_key :id
+
+    attribute :title, :string do
+      public? true
+    end
+
+    timestamps()
   end
 
   relationships do
-    has_many :messages, Hello.Chat.Message
+    has_many :messages, Hello.Chat.Message do
+      public? true
+    end
+
+    belongs_to :user, Hello.Accounts.User do
+      public? true
+      allow_nil? false
+    end
+  end
+
+  calculations do
+    calculate :needs_title, :boolean do
+      calculation expr(
+                    is_nil(title) and
+                      (count(messages) > 3 or
+                         (count(messages) > 1 and inserted_at < ago(10, :minute)))
+                  )
+    end
   end
 end
